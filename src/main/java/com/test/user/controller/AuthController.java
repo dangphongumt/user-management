af812fa8f8062bc7cce72af131893153d0222f2f
dangphong <dangphongumt@gmail.com>
2024-06-25 22:59:15 +0700

    add mappedsuperclass for audit class

---
+++ src/main/java/com/test/user/controller/AuthController.java
@@ -0,0 +1,18 @@
+package com.test.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login() {
+        return null;
+    }
+}
--- src/main/java/com/test/user/entity/Audit.java
+++ src/main/java/com/test/user/entity/Audit.java
@@ -1,10 +1,26 @@
 package com.test.user.entity;
 
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
 import java.time.Instant;
 
-public abstract class Audit {
+@MappedSuperclass
+public abstract class Audit {//implements Serializable
     private Instant createdAt;
     private Instant updatedAt;
     private String createdBy;
     private String updatedBy;
+
+    @PrePersist
+    private void onCreate() {
+        this.createdAt = Instant.now();
+        this.updatedAt = Instant.now();
+    }
+
+    @PreUpdate
+    private void onUpdate() {
+        this.updatedAt = Instant.now();
+    }
 }
